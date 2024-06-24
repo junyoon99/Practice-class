8eb8f22886ad63f1bc498da2a37b2b5a6a255b64
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2024-06-24 16:56:51 +0900

    클래스 맴버함수 포인터

--- 0619_1/main.cpp
+++ 0619_1/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+class MyClass 
+{
+public:
+	int Function(int, int) 
+	{
+		std::cout << "¤±¤·¤©";
+	}
+};
+
 int main() 
 {
-	
+	int(MyClass::*fp)(int, int) = &MyClass::Function;
+	MyClass c;
+	fp = &MyClass::Function;
+	(c.*fp)(1, 2);
 }
